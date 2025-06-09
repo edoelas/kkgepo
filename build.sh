91951ce6b95c1c0e1df101c13a959d1d42169aac
edoelas <miego.cvcv@gmail.com>
2025-06-09 14:28:43 +0200

    Update .gitignore to include requirements.txt and add build script for package management

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+uv pip compile pyproject.toml > requirements.txt
+pex -r requirements.txt -o kk.pex --exe main.py
+export KKGEPO_ALIASES=$PWD/"aliases.yaml"
+rm requirements.txt
+# Run the application
+./kk.pex gepo
