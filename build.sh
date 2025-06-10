5434dbb887c27d9adbe13580bc7d1b9c950f385b
edoelas <miego.cvcv@gmail.com>
2025-06-10 12:38:37 +0200

    Fix build script and cleanup configs

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 uv pip compile pyproject.toml > requirements.txt
 pex -r requirements.txt -o kk.pex --exe main.py
-export KKGEPO_ALIASES=$PWD/"aliases.yaml"
+export KKGEPO_ALIASES="$PWD/aliases.yaml"
 rm requirements.txt
 # Run the application
 ./kk.pex gepo
